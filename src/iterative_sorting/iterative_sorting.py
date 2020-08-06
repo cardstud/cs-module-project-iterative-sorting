479c4a3646d1c6971f4dd78d814eabf3286c926d
Todd DiFronzo <fonze@aol.com>
2020-08-05 17:30:43 -0700

    assignment completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,23 +4,50 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        smallest_value = arr[cur_index]
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for unsorted_index in range(cur_index, len(arr)):
+            if arr[unsorted_index] < smallest_value:
+                smallest_value = arr[unsorted_index]
+                smallest_index = unsorted_index
 
         # TO-DO: swap
-        # Your code here
-
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
     return arr
+
+# test it
+arr=[5, 55, 6, 67, 16, 9, 25, 43, 12, 2, 7, 88, 45, 6]
+print(selection_sort(arr))
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    # pass through the array and peform no swaps
+    swaps_occurred = True
+    # iterating through the arr and looking at elements two at a time
+    #    and swapping them if they're out of order
+    while swaps_occurred:
+        # toggle swaps_occurred boolean to False at beginning of while loop 
+        swaps_occurred = False
+    # if we do this all the way through, all the elements will
+    #    eventually end up in sorted order
+        for i in range(len(arr) - 1):    # i is the left element of the two values we are looking at
+            # we need to swap if arr[i] > arr[i+1]
+            if arr[i] > arr[i+1]:
+                # swap them
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps_occurred = True 
+    
+    return arr 
 
+# Test it
+#arr = [13, 15, 6, 18, 3, 27, 19, 22]
+arr = [5,1,7,6,2,1,0]
+bubble_sort(arr)  # dont print this, we let it finish then print below
+print(arr)
 
-    return arr
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +66,8 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
-    # Your code here
+# def counting_sort(arr, maximum=None):
+#     # Your code here
 
 
-    return arr
+#     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,34 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i 
     return -1   # not found
+
+# test it
+
+arr = [2, 6, 3, 8, 4, 55, 23, 44, 14, 8]
+print(linear_search(arr, 8))
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1 
+    while low <= high:
+        middle = (high + low) // 2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            high = middle - 1
+        else:
+            # target > arr[middle]:
+            low = middle + 1 
+    return -1 
 
-    # Your code here
+# test it
 
-
-    return -1  # not found
+arr = [3,4,6,16,26,28,52,55]    
+print(binary_search(arr, 4))  # worked, returned index of 1 where 4 is at
+print(binary_search(arr, 52)) # worked, returned index of 6 where 52 is at
+print(binary_search(arr, 8))  # worked, returned -1 since 8 not in list
+print(binary_search(arr, 6))
